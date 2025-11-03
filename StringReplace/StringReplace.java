1979103a63faf47fa9686eebfbaafbfa20ba22ee
xySaad <saadrm.dev@gmail.com>
2025-11-03 17:13:00 +0100

    StringReplace

---
+++ StringReplace/StringReplace.java
@@ -0,0 +1,9 @@
+public class StringReplace {
+    public static String replace(String s, CharSequence target, CharSequence replacement) {
+        return s.replace(target, replacement);
+    }
+
+    public static String replace(String s, char target, char replacement) {
+        return s.replace(target, replacement);
+    }
+}
