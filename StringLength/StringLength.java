0fdc850a29547957d07b04ed9f4748ceda3077af
xySaad <saadrm.dev@gmail.com>
2025-11-03 16:37:37 +0100

    fix: null case

--- StringLength/StringLength.java
+++ StringLength/StringLength.java
@@ -1,5 +1,9 @@
 public class StringLength {
     public static int getStringLength(String s) {
+        if (s == null) {
+            return 0;
+        }
+        
         return s.length();
     }
 }
