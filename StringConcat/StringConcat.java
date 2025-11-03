f2c725d11c731e76e6adce190ae78d908a069d83
xySaad <saadrm.dev@gmail.com>
2025-11-03 16:44:00 +0100

    fix: ternary operator syntax)

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,5 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        return s1 == null ? "" : s1 + s2 == null ? "" : s2;
+        return (s1 == null ? "" : s1) + (s2 == null ? "" : s2);
     }
 }
