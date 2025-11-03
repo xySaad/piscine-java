0bac902ed07aab19b21eb55f6f3b3fa5b770223d
xySaad <saadrm.dev@gmail.com>
2025-11-03 16:46:12 +0100

    fix: null concatination

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,13 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null) {
+            return s2;
+        }
+        
+        if (s2 == null) {
+            return s1;
+        }
+
         return (s1 == null ? "" : s1) + (s2 == null ? "" : s2);
     }
 }
