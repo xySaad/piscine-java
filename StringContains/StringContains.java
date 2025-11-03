f94573cd3979f96b11de96eea920a67fa6f6f72f
xySaad <saadrm.dev@gmail.com>
2025-11-03 17:02:26 +0100

    StringContains

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,5 +1,8 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
+        if (s == null) {
+            return false;
+        }
        return s.contains(subString);
     }
 }
