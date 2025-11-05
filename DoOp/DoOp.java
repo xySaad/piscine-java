bb76d5b602e752480473d137eb17ab98ea2c418a
xySaad <saadrm.dev@gmail.com>
2025-11-05 23:24:31 +0100

    -

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,5 +1,34 @@
 public class DoOp {
     public static String operate(String[] args) {
-        // your code here
+        if (args.length != 3)
+            return "Error";
+
+        int left = Integer.parseInt(args[0]);
+        int right = Integer.parseInt(args[2]);
+
+        try {
+            return Integer.toString(applyOp(args[1], left, right));
+        } catch (Error e) {
+            System.out.println(left + args[1] + right);
+            return "Error";
+        }
+    }
+
+    static int applyOp(String op, int left, int right) {
+        switch (op) {
+            case "+":
+                return left + right;
+            case "-":
+                return left - right;
+            case "*":
+                return left * right;
+            case "%":
+                return left % right;
+            case "/":
+                return left / right;
+
+            default:
+                throw new Error("unsupported operator: " + op);
+        }
     }
 }
