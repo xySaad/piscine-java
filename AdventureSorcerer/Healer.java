5c011adee8708fe2483b20a13e6eccec08d12055
xySaad <saadrm.dev@gmail.com>
2025-11-11 15:40:14 +0100

    .

---
+++ AdventureSorcerer/Healer.java
@@ -0,0 +1,5 @@
+public interface Healer {
+    public void heal(Character c);
+
+    public int getHealCapacity();
+}
---
+++ AdventureSorcerer/Monster.java
@@ -0,0 +1,12 @@
+public class Monster extends Character {
+    public Monster(String name, int maxHealth) {
+        super(name, maxHealth);
+    }
+
+    public String toString() {
+        if (this.getCurrentHealth() == 0) {
+            return String.format("%s is a monster and is dead", this.getName());
+        }
+        return String.format("%s is a monster with %d HP", this.getName(), this.getCurrentHealth());
+    }
+}
