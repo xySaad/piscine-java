9e25f77c9cfbc59b040143753644493fce346793
srm <151461794+xySaad@users.noreply.github.com>
2025-11-18 23:11:03 +0100

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,11 +1,17 @@
-mkdir srm-piscine-java
-cd srm-piscine-java
-mkdir gitea
-git clone https://learn.zone01oujda.ma/git/srm/piscine-java/ gitea
+#!/bin/bash
+
+USERNAME="java"
+PASSWORD="xxx"
+
+mkdir -p srm-piscine-java && cd srm-piscine-java
+git clone https://$USERNAME:$PASSWORD@learn.zone01oujda.ma/git/srm/piscine-java.git gitea
+
 wget https://codeload.github.com/xySaad/piscine-java/zip/refs/heads/main -O srm-java.zip
 unzip srm-java.zip
+
 cp -r piscine-java-main/* gitea
 cd gitea
+
 git add .
 git commit -m "."
 git push https://learn.zone01oujda.ma/git/srm/piscine-java/
