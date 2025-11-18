9478b35b0bfc1ef3b25efc0b50c2d4a1d381b2e0
srm <151461794+xySaad@users.noreply.github.com>
2025-11-18 22:27:12 +0100

    Create DifferenceBetweenDate.java

---
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -0,0 +1,38 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+
+public class DifferenceBetweenDate {
+
+    public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        if (localTime1 == null || localTime2 == null)
+            return null;
+
+        if (localTime1.isBefore(localTime2))
+            return Duration.between(localTime1, localTime2);
+
+        return Duration.between(localTime2, localTime1);
+    }
+
+    public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        if (date1 == null || date2 == null)
+            return null;
+
+        if (date1.isBefore(date2))
+            return Period.between(date1, date2);
+
+        return Period.between(date2, date1);
+    }
+
+    public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        if (dateTime1 == null || dateTime2 == null)
+            return null;
+
+        if (dateTime1.isBefore(dateTime2))
+            return Duration.between(dateTime1, dateTime2).toHours();
+
+        return Duration.between(dateTime2, dateTime1).toHours();
+    }
+}
